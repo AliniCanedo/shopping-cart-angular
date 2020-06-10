157b09e8795fa6feb3fa2faf33345bad52f6c679
alini <alini.canedo@gmail.com>
2020-06-09 22:17:37 -0300

    gerando os components do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LojaComponent } from './loja/loja.component';
+import { GamesComponent } from './loja/games/games.component';
+import { OrdemComponent } from './loja/ordem/ordem.component';
+import { CarrinhoDeComprasComponent } from './loja/carrinho-de-compras/carrinho-de-compras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LojaComponent,
+    GamesComponent,
+    OrdemComponent,
+    CarrinhoDeComprasComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/loja/ordem/ordem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordem',
+  templateUrl: './ordem.component.html',
+  styleUrls: ['./ordem.component.css']
+})
+export class OrdemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
